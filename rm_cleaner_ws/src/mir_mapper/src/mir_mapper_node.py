ca3ba58d2d693da615a74ef4dc104aedaa93676b
FlipGoncalves <filipeg@ua.pt>
2024-05-17 11:54:06 +0100

    mapper with saving map (not wirking yet)

--- rm_cleaner_ws/src/mir_mapper/src/mir_mapper_node.py
+++ rm_cleaner_ws/src/mir_mapper/src/mir_mapper_node.py
@@ -4,26 +4,30 @@ import rospy
 from std_msgs.msg import String
 import subprocess
 
+# Start explore_lite node for Autonomous Exploration
 def start_explore_lite():
-    # Start explore_lite node for autonomous exploration
     rospy.loginfo("Starting explore_lite for autonomous exploration...")
     subprocess.Popen(["roslaunch", "explore_lite", "explore.launch"])
 
+# Start gmapping for SLAM
 def start_slam():
-    # Start gmapping for SLAM
     rospy.loginfo("Starting gmapping for SLAM...")
     subprocess.Popen(["rosrun", "gmapping", "slam_gmapping"])
 
+# Main function
 def main():
+    # Start node
     rospy.init_node('mir_mapper_node', anonymous=True)
     rospy.loginfo("Starting MIR Mapper Node...")
 
-    # Start SLAM and exploration
+    # Start SLAM and Exploration
     start_slam()
     rospy.sleep(5)  # Wait for SLAM to initialize
     start_explore_lite()
 
     rospy.spin()
+    subprocess.Popen(["rosrun", "map_server", "map_saver", "-f", "ExplorationMap"])
+    rospy.loginfo("Saved map to ExplorationMap")
 
 if __name__ == '__main__':
     try:
